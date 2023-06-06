e1e1d25884b26be5a40692e65ee1018227fda018
Vil Pietersen <contactvil@gmail.com>
2023-06-06 11:18:53 +0100

    Committing template for project

---
+++ NeuralNetwork.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * @author Vil Pietersen
+ * @version 06/06/2023
+ */
+public class NeuralNetwork {
+
+    /**
+     * Read letter from Std Input and output corresponding square root value.
+     * @param args the command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        System.out.println("Start here");
+    }
+}
